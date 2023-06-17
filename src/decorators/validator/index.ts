0d14864444b5c805e65dcdc901c73035f12bb912
iimm <liudichen@foxmail.com>
2023-06-17 21:13:05 +0800

    +IsDateString

--- src/decorators/validator/index.ts
+++ src/decorators/validator/index.ts
@@ -1,6 +1,6 @@
 import {
-  ValidationArguments,
-  ValidationOptions,
+  type ValidationArguments,
+  type ValidationOptions,
   ValidateBy,
   isNumber,
   isString,
@@ -8,7 +8,9 @@ import {
   isInt,
   isArray,
   isBoolean,
+  buildMessage,
 } from 'class-validator';
+import isIso8601String from 'validator/lib/isISO8601'
 
 const InnerTypesValidator = {
   number: isNumber,
@@ -78,3 +80,35 @@ export const IsSelfOrArrayType = (
     },
     validationOptions,
   );
+
+    interface IsISO8601Options {
+        /**
+         * If `strict` is `true`, performs additional checks for valid dates,
+         * e.g. invalidates dates like `2009-02-29`.
+         *
+         * @default false
+         */
+        strict?: boolean | undefined;
+        /**
+         * If `strictSeparator` is true, date strings with date and time separated
+         * by anything other than a T will be invalid
+         *
+         */
+        strictSeparator?: boolean | undefined;
+    }
+
+  export const IsDateString = (options?: IsISO8601Options, validationOptions?: ValidationOptions) => 
+  ValidateBy(
+    {
+      name: 'IS_DATE_STRING',
+      constraints: [options],
+      validator: {
+        validate: (value): boolean => isIso8601String(value, options),
+        defaultMessage: buildMessage(
+          eachPrefix => eachPrefix + '$property must be a valid ISO 8601 date string',
+          validationOptions
+        ),
+      },
+    },
+    validationOptions
+  )
